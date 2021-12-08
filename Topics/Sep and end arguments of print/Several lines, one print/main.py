4ead52a9fd077ab2e0005663872691bb2a8e0ca3
rolmos14 <olmos.rafa@gmail.com>
2021-12-08 19:49:32 +0100

    Stage 3/5. start_game() method now it's run(). Run method is updated to control exit option. user_turn() is updated to print "Invalid input".

---
+++ Topics/Sep and end arguments of print/Several lines, one print/main.py
@@ -0,0 +1,6 @@
+line1 = "Night, square, apothecary, lantern,"
+line2 = "Its meaningless and pallid light."
+line3 = "Return a half a lifetime after – "
+line4 = "All will remain. A scapeless rite."
+
+print(line1, line2, line3, line4, sep="\n")
