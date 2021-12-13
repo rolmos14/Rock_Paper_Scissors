299af7f6132203d6f8bbce5e58e3ca1e2f5de24e
rolmos14 <olmos.rafa@gmail.com>
2021-12-13 23:34:35 +0100

    Stage 5/5. Class attribute valid_choices it's now default_choices and valid_choices it's now an instance attribute. Class attribute results disappears becuase now it's deduced from the valid_choices. New method select_game_options. result method updated accordingly to new rules to select win-lose options based on problem description.

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -3,24 +3,21 @@ import random
 
 class RockPaperScissors:
 
-    valid_choices = ["rock", "paper", "scissors"]
-    results = {"win": {"rock": "scissors",
-                       "paper": "rock",
-                       "scissors": "paper"},
-               "lose": {"rock": "paper",
-                        "paper": "scissors",
-                        "scissors": "rock"}}
+    default_choices = ["rock", "paper", "scissors"]
 
     def __init__(self):
         self.user_name = ""
         self.user_score = 0
         self.user_choice = ""
         self.com_choice = ""
+        self.valid_choices = self.default_choices
 
     def run(self):
         self.user_name = input("Enter your name: ")
         print(f'Hello, {self.user_name}')
         self.check_existing_score()
+        self.select_game_options()
+        print("Okay, let's start")
         while True:
             self.user_turn()
             if self.user_choice == "!exit":
@@ -39,6 +36,11 @@ class RockPaperScissors:
                 if line.startswith(self.user_name):
                     self.user_score = int(line.split()[1])
 
+    def select_game_options(self):
+        game_options = input()
+        if game_options != '':
+            self.valid_choices = game_options.split(",")
+
     def user_turn(self):
         while True:
             self.user_choice = input()
@@ -50,10 +52,17 @@ class RockPaperScissors:
         self.com_choice = random.choice(self.valid_choices)
 
     def result(self):
-        if self.results["win"][self.user_choice] == self.com_choice:
+        # Options after selected option until half of the length of the list win over
+        # selected option, in a circular list. The other half lose.
+        user_choice_index = self.valid_choices.index(self.user_choice)
+        aux_choices = self.valid_choices[user_choice_index + 1:]
+        aux_choices.extend(self.valid_choices[:user_choice_index])
+        user_lose_choices = aux_choices[:len(aux_choices) // 2]
+        user_win_choices = aux_choices[len(aux_choices) // 2:]
+        if self.com_choice in user_win_choices:
             print(f'Well done. The computer chose {self.com_choice} and failed')
             self.user_score += 100
-        elif self.results["lose"][self.user_choice] == self.com_choice:
+        elif self.com_choice in user_lose_choices:
             print(f'Sorry, but the computer chose {self.com_choice}')
         else:
             print(f'There is a draw ({self.com_choice})')
